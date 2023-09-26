f5203ec3e93e72ebdc7f29acfa65005b0e4b0061
Sriman-narayanan-S <srimansridhar4@gmail.com>
2023-09-26 15:35:19 +0530

    ok

--- git_service/views.py
+++ git_service/views.py
@@ -8,13 +8,10 @@ import git
 
 def view_git_history(request):
     try:
-        # Replace 'repo_url' with the URL of the Git repository you want to view
         repo_url = "https://github.com/Sriman-narayanan-S/newgit.git"
 
-        # Clone the Git repository to a local directory (optional)
-        # This step fetches the repository if it's not already cloned locally
-        local_repo_path = "E:\git\services\.git"  # Replace with your desired local path
-        repo = git.Repo.clone_from(repo_url, local_repo_path, branch="main")
+        # Open a connection to the remote Git repository
+        repo = git.Repo(repo_url)
 
         # Fetch the commit history
         commit_history = []
