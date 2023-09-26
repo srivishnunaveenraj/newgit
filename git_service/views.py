163b813816514b6562d20c09d794bac35f352399
Sriman-narayanan-S <srimansridhar4@gmail.com>
2023-09-26 15:49:56 +0530

    ok

--- git_service/views.py
+++ git_service/views.py
@@ -1,10 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
-# views.py
-from django.http import JsonResponse
-import git
-
 # views.py
 from django.http import JsonResponse
 import git
@@ -12,20 +5,21 @@ import git
 
 def get_git_history(request):
     try:
-        repo = git.Repo("https://github.com/Sriman-narayanan-S/newgit.git")  # Replace with the actual path to your Git repository
+        repo = git.Repo("/path/to/your/git/repository")  # Replace with the actual path to your Git repository
         commit_history = []
 
         for commit in repo.iter_commits():
+            is_merge = len(commit.parents) > 1  # Check if commit has more than one parent
             commit_info = {
                 'name': commit.author.name,
                 'message': commit.message,
                 'date': commit.authored_datetime.strftime("%Y-%m-%d"),
                 'time': commit.authored_datetime.strftime("%H:%M:%S"),
-                'sha': commit.hexsha
+                'sha': commit.hexsha,
+                'is_merge': is_merge
             }
             commit_history.append(commit_info)
 
         return JsonResponse({'history': commit_history})
     except Exception as e:
         return JsonResponse({'error': str(e)})
-
