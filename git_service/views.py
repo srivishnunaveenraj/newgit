a571f90907619e8ab264cdf4b633d23973941ac2
Sriman-narayanan-S <srimansridhar4@gmail.com>
2023-09-26 15:30:44 +0530

    Done

--- git_service/views.py
+++ git_service/views.py
@@ -7,13 +7,35 @@ import git
 
 
 def view_git_history(request):
-    try:
-        # Replace 'repo_url' with the URL of the Git repository you want to view
-        # repo_url = "https://github.com/Sriman-narayanan-S/newgit.git"
+    # try:
+    #     # Replace 'repo_url' with the URL of the Git repository you want to view
+    #     # repo_url = "https://github.com/Sriman-narayanan-S/newgit.git"
+    #     repo_url = "https://github.com/Sriman-narayanan-S/newgit.git"
+    #
+    #     # Open a connection to the remote Git repository
+    #     repo = git.Repo(repo_url)
+    #
+    #     # Fetch the commit history
+    #     commit_history = []
+    #     for commit in repo.iter_commits():
+    #         commit_info = {
+    #             "hash": commit.hexsha,
+    #             "author": commit.author.name,
+    #             "date": commit.committed_datetime.isoformat(),
+    #             "message": commit.message,
+    #         }
+    #         commit_history.append(commit_info)
+    #
+    #     return JsonResponse({"commit_history": commit_history})
+    #
+    # except Exception as e:
+    #     return JsonResponse({"error": str(e)}, status=500)
         repo_url = "https://github.com/Sriman-narayanan-S/newgit.git"
 
-        # Open a connection to the remote Git repository
-        repo = git.Repo(repo_url)
+        # Clone the Git repository to a local directory (optional)
+        # This step fetches the repository if it's not already cloned locally
+        local_repo_path = "E:\git\services\.git"  # Replace with your desired local path
+        repo = git.Repo.clone_from(repo_url, local_repo_path, branch="main")
 
         # Fetch the commit history
         commit_history = []
