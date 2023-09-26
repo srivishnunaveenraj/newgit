cd9f2f30d95400febf4376e0acb0e931ccd386fa
Sriman-narayanan-S <srimansridhar4@gmail.com>
2023-09-26 15:33:08 +0530

    Done

--- git_service/views.py
+++ git_service/views.py
@@ -7,29 +7,8 @@ import git
 
 
 def view_git_history(request):
-    # try:
-    #     # Replace 'repo_url' with the URL of the Git repository you want to view
-    #     # repo_url = "https://github.com/Sriman-narayanan-S/newgit.git"
-    #     repo_url = "https://github.com/Sriman-narayanan-S/newgit.git"
-    #
-    #     # Open a connection to the remote Git repository
-    #     repo = git.Repo(repo_url)
-    #
-    #     # Fetch the commit history
-    #     commit_history = []
-    #     for commit in repo.iter_commits():
-    #         commit_info = {
-    #             "hash": commit.hexsha,
-    #             "author": commit.author.name,
-    #             "date": commit.committed_datetime.isoformat(),
-    #             "message": commit.message,
-    #         }
-    #         commit_history.append(commit_info)
-    #
-    #     return JsonResponse({"commit_history": commit_history})
-    #
-    # except Exception as e:
-    #     return JsonResponse({"error": str(e)}, status=500)
+    try:
+        # Replace 'repo_url' with the URL of the Git repository you want to view
         repo_url = "https://github.com/Sriman-narayanan-S/newgit.git"
 
         # Clone the Git repository to a local directory (optional)
